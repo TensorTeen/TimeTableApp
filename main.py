ce1d9090a4def79d758a3ca701db9f631b12b6f2
catdisk04 <aldisdaniel04@gmail.com>
2023-07-31 16:31:50 +0530

    Basic implementation done

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
-from APITest import createEvent
+#from APITest import createEvent
 import json
+from icalendar import Calendar, Event
 import datetime
 
 #iso stansard weeks start on monday. can assign 0 or , 1 better.
@@ -16,10 +17,10 @@ days = {
 
 
 class Slot():
-    def __init__(self, slotName: str, slotTimes: np.array, course=None, rec="WEEKLY") -> None: #I suggest slotTimes as type WeekTime
+    def __init__(self, slotName: str, slotTimes: "np.array of TimeRange", course=None, rec="WEEKLY") -> None: #I suggest slotTimes as type WeekTime
         self.slotName = slotName
         self.slotTimes = slotTimes
-        self.course = course 
+        self.course     = course 
         self.rec = rec
         if self.course:
             self.occupied = True
@@ -42,23 +43,48 @@ class Slot():
             json.dump(details, f, indent=6)
 
 class WeekTime():
-    def __init__(self, day:int, time: datetime.time):
+    def __init__(self, day:int, time: datetime.time ):
         self.day = day
         self.time = time
+    def getDay(self):
+        return self.day
+    def getTime(self):
+        return self.time
+    
+class TimeRange(WeekTime):
+    def __init__(self, day:int, start:datetime.time, end:datetime.time):
+        self.day = day
+        self.time = start
+        self.startTime = start
+        self.endTime = end
+    def getStart(self):
+        return self.startTime
+    
+    def getEnd(self):
+        return self.endTime
+
+class Rule():
+    def __init__(self, condition, contents: dict):
+        self.content = contents
+        self.condition =condition
+    
+    
 
 class Course():
-    def __init__(self, courseName: str, slot: Slot, loc="", desc=None, rules=[]) -> None:
+    def __init__(self, courseName: str, slot: Slot, loc="", desc="", rules=[]) -> None:
         self.courseName = courseName
         self.slot = slot
-        self.loc = loc
-        self.desc = desc
-        self.attrs = ['loc', 'desc']
+        self.attrs = {
+            "name" : courseName, 
+            "loc" : loc, 
+            "desc" : desc
+            }
         self.rules = rules
 
     def __repr__(self) -> str:
         return self.courseName
 
-    def retrieveTime(self, day: int) -> np.array:
+    def retrieveTime(self, day: int) -> np.array: 
         return self.slot.retrieveTime(day)
 
     def retrieveLocation(self) -> str:
@@ -82,16 +108,46 @@ class Course():
 #implement conditions in courses
 #need to convert calendar object to ical, converting recurrent events to actual dates and times
 
-class Caleder():
-    def __init__(self, start: datetime.day, end: datetime.day, courses = []) -> None:
+class Calendar():
+    def __init__(self, start: datetime.date, end: datetime.date, courses = [], icalobj = Calendar()) -> None:
         self.start = start
-        self.end = self.end
+        self.end = end
         self.courses = courses
+        self.icalObj = icalobj #TODO
     
     def addCourse(self, course: Course) -> None:
         self.courses.append(Course)
     
-    def createEvents(course):
+    def saveIcal(self, name = "cal"):
+        with open(name+".ical", "w") as calFile:
+            calFile.write(self.icalObj.to_ical().decode("utf-8"))
+    
+    def createEvents(self, course):
+        day = self.start
+        while day != self.end + datetime.timedelta(days=1):
+            #for each day check every slot
+            for slot in course.slot.slotTimes:
+                if slot.getDay() == day.isoweekday():
+                    #add event
+                    #TODO implement rule
+                    event = Event()
+                    event.add('summary', course.attrs["name"])
+                    event.add('location', course.attrs["loc"])
+                    event.add('description', course.attrs["desc"])
+                    event.add("dtstart", datetime.datetime.combine(day, slot.getStart()))
+                    event.add("dtend", datetime.datetime.combine(day, slot.getEnd()))
+                    self.icalObj.add_component(event)
+            
+            day += datetime.timedelta(days = 1)
+            print(day)
+        pass
+        #TODO
+
+# Example usage
+# slot = Slot("A", [TimeRange(1, datetime.time(10, 0), datetime.time(10, 50))])
+# course = Course("MA", slot)
+# calendar= Calendar(datetime.date(2024, 7, 31), datetime.date(2024, 8, 10), [course] )
+# calendar.createEvents(calendar.courses[0])
         
         
         
---
+++ rough.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 31 14:39:06 2023
+
+@author: aldis
+"""
+
+import datetime
+import
